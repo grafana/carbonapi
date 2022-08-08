2e6d277ec5cc9515b5250ddbbfdfef4a11062f72
Nicolás Pazos <npazosmendez@gmail.com>
2022-08-08 18:09:24 -0300

    Fix build and some tests

--- expr/functions/groupByNode/function_test.go
+++ expr/functions/groupByNode/function_test.go
@@ -168,7 +168,7 @@ func TestGroupByNode(t *testing.T) {
 				"metric1.foo.bar1.bla.foo": {types.MakeMetricData("metric1.foo.bar1.bla.foo", []float64{1, 2, 3, 4, 5}, 1, now32)},
 			},
 		},
-		// FIXME: I don't think `..foo==.bar` is a valid metric name, that is making these tests fail
+		// FIXME: I don't think `..foo==.bar` is a valid metric name, that is making these testsapi/cmd/carbonapi/ht fail
 		// {
 		// 	Target: "groupByNode(metric1.foo.*.*,2,\"sum\")",
 		// 	M: map[parser.MetricRequest][]*types.MetricData{
